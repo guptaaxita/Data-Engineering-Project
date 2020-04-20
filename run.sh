7c1a911b5720602a2cd3676be6e466c8652805ba
axitagupta <guptaaxita@gmail.com>
2020-04-20 03:25:02 -0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # The script works for linux based systems
 
 # Run the code for sample data and whole data
-python ./src/consumer_complaints.py './input/' './output/' './insight_testsuite/test_input/' './insight_testsuite/test_output/'
+python ./src/consumer_complaints.py ./input/ ./output/ ./insight_testsuite/test_input/ ./insight_testsuite/test_output/
 
 # For testing results against known output
-python ./insight_testsuite/consumer_complaints_test.py './insight_testsuite/test_output/'
+python ./insight_testsuite/consumer_complaints_test.py ./insight_testsuite/test_output/
 
