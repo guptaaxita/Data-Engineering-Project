a65c2c0ed512073020b39d85f61414c74ddec227
axitagupta <guptaaxita@gmail.com>
2020-04-20 02:36:34 -0400

    modified code structure

--- insight_testsuite/consumer_complaints_test.py
+++ insight_testsuite/consumer_complaints_test.py
@@ -6,19 +6,10 @@ import csv
 from datetime import datetime
 
 if sys.platform == 'win32':
-    input_path = os.getcwd() + '\\insight_testsuite\\test_input\\'
     output_path = os.getcwd() + '\\insight_testsuite\\test_output\\'
-    sys.path.insert(1, os.getcwd() + '\\src\\')
 elif sys.platform == 'linux':
-    input_path = os.getcwd() + '/insight_testsuite/test_input/'
     output_path = os.getcwd() + 'insight_testsuite/test_output/'
-    sys.path.insert(1, os.getcwd() + '/src/')
     
-import IDE_lib as de_lib
-
-# Create report_test.csv from a sample test data for consumer complaints
-de_lib.create_reports(input_path, output_path, 'complaints_test.csv', 'report_test.csv')
-
 # Importing report_test.csv to test results are correct
 file=open(output_path+'report_test.csv','r')
 reader=csv.reader(file)
@@ -27,16 +18,30 @@ reader=csv.reader(file)
 test_result_1 = ["debt collection",'2020','2','4','50']
 test_result_2 = ["payday loan, title loan, or personal loan",'2019','1','1','100']
 test_result_3 = ["credit card or prepaid card",'2019','4','4','100']
-test_result_4 = ["debt collection",2020,2,4,50]
+test_result_4 = ["debt collection",'2020','2','4','50']
 
 answer_list = []
 for i, row in enumerate(reader):
     answer_list.append(row)
 
-de_lib.test_one_scenario(answer_list, test_result_1, '1')
-de_lib.test_one_scenario(answer_list, test_result_2, '2')
-de_lib.test_one_scenario(answer_list, test_result_3, '3')
-de_lib.test_one_scenario(answer_list, test_result_4, '4')
+def test_one_scenario(answer_list, test_result_list, test_name):
+    
+    '''
+    Defining a test function to test and compare results against the known results.
+    
+    '''
+    
+    if len(test_result_list) != 5:
+        sys.exit('Incorrect number of values in test scenario, no test can be executed')
+    
+    if test_result_list in answer_list:
+        print('Test scenario {} passed'.format(test_name))
+    else:
+        print('Test failed: Wrong output generated for product: {}, year: {}'.format(test_result_list[0], 
+                                                                                     test_result_list[1]))
 
-
-
+# Run all test scenarios:
+test_one_scenario(answer_list, test_result_1, '1')
+test_one_scenario(answer_list, test_result_2, '2')
+test_one_scenario(answer_list, test_result_3, '3')
+test_one_scenario(answer_list, test_result_4, '4')
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-# First file will be used to test and compare results and second file will be used to run complete dataset
 # The script works for linux based systems
 
-# For testing results 
+# Run the code for sample data and whole data
+python ./src/consumer_complaints.py
+
+# For testing results against known output
 python ./insight_testsuite/consumer_complaints_test.py
 
-# For large file (whole dataset)
-python ./src/consumer_complaints.py
