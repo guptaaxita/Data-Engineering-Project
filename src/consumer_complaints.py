47a9a9f3c81dbe589e92254f765bc1a18b8db266
axitagupta <guptaaxita@gmail.com>
2020-04-20 03:16:51 -0400

    fixed run.sh

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -15,6 +15,8 @@ else:
     output_path = sys.argv[2]
     test_input_path = sys.argv[3]
     test_output_path = sys.argv[4]
+    
+print('Total number of arguments:', len(sys.argv))
 
 def check_filename(filename):
     
